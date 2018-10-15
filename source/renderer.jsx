82fbcb99520c6bca986958a359572886bcc036e5
Rue Yokaze <yokaze.rue@gmail.com>
2018-10-15 22:18:30 +0900

    mockup

--- source/renderer.jsx
+++ source/renderer.jsx
@@ -4,7 +4,17 @@ import ReactDOM from 'react-dom';
 const cols = [];
 for (let i = 0; i < 16; ++i)
 {
-  cols.push(<p key={i}>{i}</p>);
+    const style = {
+        display: "inline-block",
+        fontFamily: "Monaco, monospace",
+        fontSize: "16px",
+        height: "24px",
+        lineHeight: "24px",
+        textAlign: "center",
+        width: "32px"
+    };
+    const title = '+' + i.toString(16).toUpperCase();
+    cols.push(<span key={i} style={style}>{title}</span>);
 }
 const content = <div>{cols}</div>;
 
