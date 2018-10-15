185015b64793b8a9fe0b9a2ad99af6dca01d3f7a
Rue Yokaze <yokaze.rue@gmail.com>
2018-10-15 20:51:56 +0900

    mockup

--- source/main.js
+++ source/main.js
@@ -1,9 +1,15 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
 
-app.on('ready', function()
-{
+app.on('ready', function() {
     const browserWindow = new BrowserWindow();
-    browserWindow.show();
+    const indexUrl = url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    });
+    browserWindow.loadURL(indexUrl);
 });
