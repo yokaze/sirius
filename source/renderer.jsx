5ecf78ff207c29ddb10991d852592b4c8867052b
Rue Yokaze <yokaze.rue@gmail.com>
2018-10-18 21:15:54 +0900

    mockup

--- source/renderer.jsx
+++ source/renderer.jsx
@@ -21,43 +21,71 @@ const style = {
     width: "32px"
 };
 
-class BinaryTableCell extends Component {
-    constructor(props, context, updater) {
-        super(props, context, updater);
-        this.address = props.address;
-        this.value = props.value;
+class BinaryTableRow extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            address: props.address,
+        };
     }
     render() {
-        const text = ('00' + this.value.toString(16)).slice(-2).toUpperCase();
-        return <span key={this.address} style={style} tabIndex={this.address} onKeyDown={this.handleKeyDown}>{text}</span>;
+        const address = this.state.address;
+        const text = ('00000000' + address.toString(16)).slice(-8).toUpperCase();
+        return <span key={"address" + address} style={addressStyle}>{text}</span>;
+    }
+};
+
+class BinaryTableCell extends Component {
+    constructor(props) {
+        super(props);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+    render() {
+        const text = ('00' + this.props.value.toString(16)).slice(-2).toUpperCase();
+        return <span key={this.props.address} style={style} tabIndex={this.props.address} onKeyDown={this.handleKeyDown}>{text}</span>;
     }
     handleKeyDown(e) {
-        console.log(e);
+        this.props.handleKeyDown(e);
     }
 }
 
-content.push(<span key="address" style={addressStyle}>Address</span>);
-for (let i = 0; i < 16; ++i)
-{
-    const title = '+' + i.toString(16).toUpperCase();
-    content.push(<span key={i} style={style}>{title}</span>);
-}
-content.push(<br key={"br"} />);
-for (let j = 0; j < 20; ++j)
-{
-    const address = j * 16;
-    const title = ('00000000' + address.toString(16)).slice(-8).toUpperCase();
-    content.push(<span key={"address" + j} style={addressStyle}>{title}</span>);
-    for (let i = 0; i < 16; ++i)
-    {
-        const address = i + j * 16;
-        const value = (i + j * 16) % 256;
-        content.push(<BinaryTableCell key={"btcell" + address} address={address} value={value} />);
+class BinaryTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            startAddress: 0
+        };
+        this.handleKeyDown = this.handleKeyDown.bind(this);
     }
-    content.push(<br key={"br" + j} />);
-}
+    render() {
+        const items = [];
+        items.push(<span key="address" style={addressStyle}>Address</span>);
+        for (let i = 0; i < 16; ++i)
+        {
+            const title = '+' + i.toString(16).toUpperCase();
+            items.push(<span key={i} style={style}>{title}</span>);
+        }
+        items.push(<br key="br-head" />);
+        for (let j = 0; j < 20; ++j)
+        {
+            const row = <BinaryTableRow key={"btrow" + j + this.state.startAddress} address={j * 16 + this.state.startAddress} />;
+            items.push(row);
+            for (let i = 0; i < 16; ++i)
+            {
+                const address = i + j * 16 + this.state.startAddress;
+                const value = (address) % 256;
+                const cell = <BinaryTableCell key={"btcell" + address} address={address} value={value} handleKeyDown={this.handleKeyDown} />;
+                items.push(cell);
+            }
+            items.push(<br key={"br" + j} />);
+        }
+        return <div className="binary-table">{items}</div>;
+    }
+    handleKeyDown(e) {
+        this.setState((state, props) => ({ startAddress: state.startAddress + 0x10 }));
+    }
+};
 
-ReactDOM.render(
-    <div className="binary-table">{content}</div>,
+ReactDOM.render(<BinaryTable />,
     document.getElementById('root')
 );
