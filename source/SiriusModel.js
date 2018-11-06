73cf250e17e9163c6b5cc02d52788ea9e9ade2ed
Rue Yokaze <yokaze.rue@gmail.com>
2018-11-06 22:00:25 +0900

    mockup

---
+++ source/SiriusModel.js
@@ -0,0 +1,21 @@
+
+export class SiriusModel {
+  constructor() {
+  }
+
+  createNew() {
+    console.log('Create New');
+  }
+
+  open() {
+    console.log('Open');
+  }
+
+  save() {
+    console.log('Save');
+  }
+
+  saveAs() {
+    console.log('Save As');
+  }
+}
--- source/main.js
+++ source/main.js
@@ -1,8 +1,38 @@
+require('babel-register');
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 const path = require('path');
 const url = require('url');
+const SiriusModel = require('./SiriusModel');
+
+const model = new SiriusModel.SiriusModel;
+
+const menuTemplate = [
+  {
+    label: app.getName(),
+    submenu: [
+      {role: 'quit'}
+    ]
+  },
+  {
+    label: 'File',
+    submenu: [
+      { label: 'New', click: model.createNew, accelerator:'CmdOrCtrl+N' },
+      { label: 'Open', click: model.open, accelerator:'CmdOrCtrl+O' },
+      { label: 'Save', click: model.save, accelerator:'CmdOrCtrl+S' },
+      { label: 'Save As', click: model.saveAs, accelerator:'CmdOrCtrl+Shift+S' }
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {role: 'undo'},
+      {role: 'redo'}
+    ]
+  },
+];
 
 app.on('ready', function() {
     const browserWindow = new BrowserWindow({ width: 1600, height: 1024 });
@@ -13,4 +43,7 @@ app.on('ready', function() {
     });
     browserWindow.loadURL(indexUrl);
     browserWindow.webContents.openDevTools();
+
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
 });
