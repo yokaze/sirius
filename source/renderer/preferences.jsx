757e81692aa2979e156f2b690114f996b25e3e20
Rue Yokaze <yokaze.rue@gmail.com>
2018-12-16 16:30:27 +0900

    send preference

--- source/renderer/preferences.jsx
+++ source/renderer/preferences.jsx
@@ -1,4 +1,4 @@
-import { remote } from 'electron';
+/* eslint-env browser */
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import FormControl from '@material-ui/core/FormControl';
@@ -10,23 +10,65 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 
 import SiriusIpcClient from '../ipc/SiriusIpcClient';
 
-const appConsole = remote.getGlobal('console');
+const defaultFontFamily = 'Roboto Mono';
+const defaultFontSize = 16;
+const defaultColumnUnit = 4;
 const ipcClient = new SiriusIpcClient();
+
+function sendPreference(state) {
+  let fontFamily = state.fontFamily;
+  let fontSize = parseInt(state.fontSize, 10);
+  const columnUnit = state.columnUnit;
+  if (fontFamily === '') {
+    fontFamily = defaultFontFamily;
+  }
+  if (isNaN(fontSize)) {
+    fontSize = defaultFontSize;
+  }
+  ipcClient.sendPreferenceCommand({
+    fontFamily,
+    fontSize,
+    columnUnit,
+  });
+}
 
 class PreferencesView extends Component {
   constructor(props) {
     super(props);
+    this.fontFamilyInputChanged = this.fontFamilyInputChanged.bind(this);
+    this.fontSizeInputChanged = this.fontSizeInputChanged.bind(this);
     this.columnUnitGroupChanged = this.columnUnitGroupChanged.bind(this);
-    this.state = { columnUnit: 16 };
+    this.state = {
+      fontFamily: '',
+      fontSize: '',
+      columnUnit: defaultColumnUnit,
+    };
+  }
+
+  fontFamilyInputChanged(e) {
+    const value = e.target.value;
+    this.setState((state) => {
+      const diff = { fontFamily: value };
+      sendPreference(Object.assign(state, diff));
+      return diff;
+    });
+  }
+
+  fontSizeInputChanged(e) {
+    const value = e.target.value;
+    this.setState((state) => {
+      const diff = { fontSize: value };
+      sendPreference(Object.assign(state, diff));
+      return diff;
+    });
   }
 
   columnUnitGroupChanged(e) {
-    const value = e.target.value;
-    this.setState(() => {
-      ipcClient.sendPreferenceCommand({
-        columnUnit: value,
-      });
-      return { columnUnit: value };
+    const value = parseInt(e.target.value, 10);
+    this.setState((state) => {
+      const diff = { columnUnit: value };
+      sendPreference(Object.assign(state, diff));
+      return diff;
     });
   }
 
@@ -34,17 +76,17 @@ class PreferencesView extends Component {
     return (<div style={{ margin: 8 }}>
       <FormControl>
         <FormLabel component="legend">Font Family</FormLabel>
-        <Input placeholder="Roboto Mono" />
+        <Input placeholder={defaultFontFamily} onChange={this.fontFamilyInputChanged} />
       </FormControl>
       <FormControl>
         <FormLabel component="legend">Font Size</FormLabel>
-        <Input placeholder="16" />
+        <Input placeholder={`${defaultFontSize}`} onChange={this.fontSizeInputChanged} />
       </FormControl>
       <FormControl>
         <FormLabel component="legend">Column Unit</FormLabel>
         <RadioGroup
           row
-          value={this.state.columnUnit.toString(10)}
+          value={`${this.state.columnUnit}`}
           onChange={this.columnUnitGroupChanged}
         >
           <FormControlLabel value="1" control={<Radio />} label="1" />
